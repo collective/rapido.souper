4547b660040b225c968692133f0041fb8c5be03e
ebrehault <ebrehault@gmail.com>
2013-10-21 22:27:39 +0200

    initial commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+rapido.souper
+=============
+
+rapido.souper provides a Souper-based persistence layer for Rapido applications.
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='rapido.souper',
+      version=version,
+      description="Souper persistence for Rapido",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['rapido'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'souper',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
